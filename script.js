3aee5fa6d33c8f4aabc957107328590cfcf93bd5
PolacyRodacy <michalpoplawski09@gmail.com>
2025-03-14 19:56:41 +0100

    navbar animation

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    // Po załadowaniu strony, wysuwamy navbar z góry
+    const navbar = document.getElementById('navbar');
+    navbar.style.top = '0'; // Przesuwamy navbar na górę ekranu
+  });
+  
